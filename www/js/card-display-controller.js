21fc0a2f9e31189e31a91f00023aed211d3087bb
Charles Liu <dodoinblue@gmail.com>
2016-05-05 17:17:36 -0400

    buttons try 1

---
+++ www/js/card-display-controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Charles on 5/4/16.
+ */
+'use strict';
+
+var ydCardDisplayCtrl = angular.module('ydCardDisplayCtrl', []);
+
+ydCardDisplayCtrl.controller('ydCardDisplayCtrl', [
+  function () {
+    var enterEditMode = function () {
+      
+    }
+  }]);
