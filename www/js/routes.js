103600af5af65eb5322ff4e180e7b5a2f5fe4ab1
Charles Liu <dodoinblue@gmail.com>
2017-02-28 09:04:17 -0800

    remove hard-coded cardtree from service

--- www/js/routes.js
+++ www/js/routes.js
@@ -16,14 +16,21 @@ ydRoutes.config(['$stateProvider', '$urlRouterProvider',
       // this page is for display cards & rearrange cards (including changing effects)
       .state('display', {
         url: '/display',
-        templateUrl: 'templates/card-display.html'
+        templateUrl: 'templates/card-display.html',
+        resolve: {
+          // This data is not needed in controller. Just a way of making sure
+          // CardTree is loaded before any call request from controller.
+          'CardsTreeData':function(ydCardService){
+            return ydCardService.promise;
+          }
+        }
       })
 
       // This page handles the creation/edition of cards and card categories
       .state('resource', {
         url: '/resource',
         templateUrl: 'templates/card-resource.html'
-      })
+      });
 
       // // Temporary
       // .state('drawer', {
